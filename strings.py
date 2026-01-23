b54ad8a9e672c54b4197b1f57369067b51e48c84
upohaar <sumaiyanobiupohaar@gmail.com>
2026-01-24 02:33:36 +0600

    strings2

---
+++ strings.py
@@ -0,0 +1,24 @@
+"""print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+print("he is 'shy' ")"""
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+#print(a)
+
+a = "Hello, World!"
+#print(a[10])
+
+for x in '':
+  print(x)
+
+
+print(len(a))
+
+
+txt = "The best things in life are free!"
+print("x" in txt)
