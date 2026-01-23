8a804252899a29ab6eff23bcff05b21a443c8a26
upohaar <sumaiyanobiupohaar@gmail.com>
2026-01-24 01:28:24 +0600

    output

---
+++ output.py
@@ -0,0 +1,10 @@
+print("upo")
+print ("haar")
+
+#end: pthon auto new line e chole jay, end dile porer line old line er sathe bosbe
+print ("upo", end="")
+print("haar")
+
+print(2+3)
+print("i'm",25,"years old")
+
